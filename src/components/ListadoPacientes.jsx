662b41e60d4eccc4af412f8cccb32467f4595aa5
raquellvazquez <raquel.skrats@gmail.com>
2022-02-23 20:34:24 -0600

    proyecto de citas con tailwind

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,42 @@
+import Paciente from "./Paciente";
+const ListadoPacientes = ({pacientes, setPaciente, eliminarPaciente}) => {
+    return (
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll ">
+            {
+                pacientes && pacientes.length ? (
+                    <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl text-center mt-5 mb-10">
+                        Administra tus {""}
+                        <span className="text-indigo-600 text-bold">Pacientes y Citas</span>
+                    </p>
+        
+        
+                    {pacientes.map((paciente) => (
+                        <Paciente
+                            key={paciente.id}
+                            paciente={paciente}
+                            setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
+                        />
+                    ))}
+                    </>
+                )
+                :
+                (
+                    <>
+                    <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+                    <p className="text-xl text-center mt-5 mb-10">
+                        Agrega un nuevo  {""}
+                        <span className="text-indigo-600 text-bold">Paciente</span>
+                    </p>
+                    </>
+                )
+            }
+           
+            
+        </div>
+    )
+}
+
+export default ListadoPacientes;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //purge: ["./index.html", "./src/**/*.{jsx}"],
+  content: [ "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
